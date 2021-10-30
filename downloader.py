799d24ab07e44aa418e341e40b4a7438fd989bd7
ShawnDong98 <ShawnDong98@gmail.com>
2021-10-30 16:45:15 +0800

    commit

--- downloader.py
+++ downloader.py
@@ -4,6 +4,6 @@ from CVPRHelper import CVPRHelper
 
 if __name__ == '__main__':
     kw = sys.argv[1]    
-    helper = CVPRHelper(2020)
+    helper = CVPRHelper(2021)
     print(f"Searching for \"{kw}\"...")
     helper.download_keyword(kw)
